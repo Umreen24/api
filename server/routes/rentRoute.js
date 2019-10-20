eb714833c7e709fb246b201ade3afd7ce6f87c3a
Umreen <uidigitalcrafts@gmail.com>
2019-10-20 17:58:18 -0500

    added api call limit

--- server/routes/rentRoute.js
+++ server/routes/rentRoute.js
@@ -3,32 +3,60 @@ const rentRoute = express.Router()
 const {check, validationResult} = require('express-validator')
 
 //db config
-const pgp = require('pg-promise')()
-const connectionString = process.env.DATABASE_URL
-const db = pgp(connectionString)
+const { Client } = require('pg')
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
+
+client.connect()
 
 rentRoute.get('/', [
-    // check('compare', 'Incorrect string entered').isIn(['greater', 'less']),
-    check('rent', 'Rent must be a number').isInt()
-    // check('quantity', 'Must be a number').isInt()
-    // check('key', 'Invalid API key').equals(process.env.DB_KEY)
+    check('compare').isIn(['greater', 'less']).withMessage('Incorrect comparison string entered'),
+    check('avgRent').isInt().withMessage('Rent must be a number'),
+    check('quantity').isInt().withMessage('Quantity must be a number'),
+    check('key').isIn(process.env.API_CLIENT_KEY.split(',')).withMessage('invalid client key')
 
 ], (req, res) => {
 
     const inputError = validationResult(req)
-    if(!inputError.isEmpty()){
-        res.status(400).json({errors: inputError.array()})
+    
+    if (!inputError.isEmpty()) {
+        return res.status(422).json({ errors: inputError.array()})
     }
 
-    // const compare = req.query.compare
-    const rent = req.query.rent
-    // const quantity = req.query.quantity
-    // compare === 'greater' ? compare === '>' : compare === '<'
-    console.log(rent)
+    const compare = req.query.compare
+    const avgRent = req.query.avgRent
+    const quantity = req.query.quantity
+    const key = req.query.key
+    const date_format = new Intl.DateTimeFormat('en-us', { dateStyle: 'short' })
 
-    db.any(`SELECT city, state, avg_rent FROM city_rents WHERE avg_rent > $1 LIMIT 5`, [rent])
-    .then(city_rents => city_rents[0] ? res.json(city_rents) : res.json({errors: 'No cities matched your rent criteria'}))
-    .catch(() => res.json({errors: 'No cities matched your rent criteria'}))
+    const current_date = date_format.format(new Date())
+
+    let compareQuery
+
+    client.query(`SELECT to_char(last_date :: DATE, 'mm/dd/yyyy') as last_date, api_calls FROM api_key_limit WHERE key = '${key}'`)
+    .then(d => {
+        let api_last_date = d.rows[0].last_date
+        let api_calls = d.rows[0].api_calls
+        if (api_last_date != current_date)
+        {
+            client.query(`UPDATE api_key_limit SET api_calls = 1, last_date = '${current_date}' WHERE key = '${key}'`)
+        }
+        else if (api_last_date === current_date && api_calls < process.env.API_CLIENT_MAX)
+        {
+            client.query(`UPDATE api_key_limit SET api_calls = api_calls + 1 WHERE key = '${key}'`)
+        }
+        else {
+            return res.status(422).json({ errors: 'API Limit Reached'}) 
+        }
+    })
+    
+    compare === 'greater' ? compareQuery = '>' : compareQuery = '<'
+
+   client.query(`SELECT city, state, avg_rent FROM city_rents WHERE avg_rent ${compareQuery} ${avgRent} LIMIT ${quantity}`)
+    .then(result => res.json(result))
+    .catch((e) => console.log(e))
 })
 
 module.exports = rentRoute
